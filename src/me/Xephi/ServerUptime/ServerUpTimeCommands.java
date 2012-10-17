79e832596a630e900a7ebe705cc2caa9b6aa300e
Alexandre V. <xephirot59@gmail.com>
2012-10-17 18:07:25 +0200

    Add Config file

--- src/me/Xephi/ServerUptime/ServerUpTimeCommands.java
+++ src/me/Xephi/ServerUptime/ServerUpTimeCommands.java
@@ -1,25 +1,24 @@
 package me.Xephi.ServerUptime;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class ServerUpTimeCommands implements CommandExecutor {
+public class ServerUptimeCommands implements CommandExecutor {
     private final ServerUptime plugin;
 
-    public ServerUpTimeCommands(ServerUptime plugin) {
+    public ServerUptimeCommands(ServerUptime plugin) {
         this.plugin = plugin;
     }
     private final long serverStart = System.currentTimeMillis();
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         final String commandName = cmd.getName().toLowerCase();
         final long diff = System.currentTimeMillis() - serverStart;
-        final String msg = " " + (int)(diff / 86400000) + " days " + (int)(diff / 3600000 % 24) + " hours " + (int)(diff / 60000 % 60) + " mins " + (int)(diff / 1000 % 60) + " seconds";
+        final String msg = " " + (int)(diff / 86400000) + " " + plugin.getConfig().get("days").toString() + " " + (int)(diff / 3600000 % 24) + " " + plugin.getConfig().get("hours").toString() +" " + (int)(diff / 60000 % 60) + " " + plugin.getConfig().get("minutes").toString() +" " + (int)(diff / 1000 % 60) + " " + plugin.getConfig().get("seconds").toString() + ".";
         if (commandName.equals("uptime")) {
             plugin.getLogger().info("[CONSOLE]" + msg);
-            sender.sendMessage(ChatColor.YELLOW + "[ServerUpTime]" + msg);
-		}
+            sender.sendMessage(ServerUptime.replaceWithChatColor("*" + plugin.getConfig().get("color").toString().toLowerCase() + "*[" + plugin.getConfig().get("prefix").toString() + "]" + msg));
+        }
         return true;
     }
 }
--- src/me/Xephi/ServerUptime/ServerUptime.java
+++ src/me/Xephi/ServerUptime/ServerUptime.java
@@ -1,20 +1,62 @@
 package me.Xephi.ServerUptime;
- 
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
- 
+
 public class ServerUptime extends JavaPlugin
 {
 
-	public ServerUpTimeCommands CommandExecutor = new ServerUpTimeCommands(this);
-    @Override
-    public void onEnable() {
-        getLogger().info("ServerUptime v1.0 enabled");
-		getCommand("uptime").setExecutor(CommandExecutor);
-
+   public static Configuration config;
+   public final Logger log = Logger.getLogger("Minecraft");
+   public ServerUptimeCommands CommandExecutor = new ServerUptimeCommands(this);
+    
+   @Override
+   public void onEnable() {
+        if(!new File(getDataFolder()+File.separator+"config.yml").exists()){
+            getConfig().options().copyDefaults(true);
+            saveDefaultConfig();
+            log.info(String.format("[ServerUptime] Copy default configuration..."));
+            log.info(String.format("[ServerUptime] Default configuration copied and loaded"));
+        }else{
+        	getConfig().options().copyDefaults(true);
+            log.info(String.format("[ServerUptime] Configuration loaded"));
+        }
+        getLogger().info("ServerUptime v1.1 enabled");
+        getCommand("uptime").setExecutor(CommandExecutor);
     }
- 
+
     @Override
     public void onDisable() {
-        getLogger().info("ServerUptime v1.0 disabled");
+        getLogger().info("ServerUptime v1.1 disabled");
     }
+
+
+	public static String replaceWithChatColor(String colorFix) {
+
+		colorFix = colorFix.replace("*dark_green*", ChatColor.DARK_GREEN.toString());
+		colorFix = colorFix.replace("*dark_red*", ChatColor.DARK_RED.toString());
+		colorFix = colorFix.replace("*dark_aqua*", ChatColor.DARK_AQUA.toString());
+		colorFix = colorFix.replace("*dark_blue*", ChatColor.DARK_BLUE.toString());
+		colorFix = colorFix.replace("*dark_gray*", ChatColor.DARK_GRAY.toString());
+		colorFix = colorFix.replace("*dark_purple*", ChatColor.DARK_PURPLE.toString());
+
+		colorFix = colorFix.replace("*red*", ChatColor.RED.toString());
+		colorFix = colorFix.replace("*gold*", ChatColor.GOLD.toString());
+		colorFix = colorFix.replace("*orange*", ChatColor.GOLD.toString());
+		colorFix = colorFix.replace("*green*", ChatColor.GREEN.toString());
+		colorFix = colorFix.replace("*aqua*", ChatColor.AQUA.toString());
+		colorFix = colorFix.replace("*white*", ChatColor.WHITE.toString());
+		colorFix = colorFix.replace("*blue*", ChatColor.BLUE.toString());
+		colorFix = colorFix.replace("*black*", ChatColor.BLACK.toString());
+		colorFix = colorFix.replace("*gray*", ChatColor.GRAY.toString());
+		colorFix = colorFix.replace("*light_purple*", ChatColor.LIGHT_PURPLE.toString());
+		colorFix = colorFix.replace("*yellow*", ChatColor.YELLOW.toString());
+		colorFix = colorFix.replace("*magic*", ChatColor.MAGIC.toString());
+
+		return colorFix;
+	}
 }
